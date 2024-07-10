41efa9e7112c0be4d6f7cc7260e90e860199b811
ronitjena09 <ronitjena64@gmail.com>
2024-07-10 10:59:34 +0530

    Created Filters, Search Button

--- Frontend/src/pages/ProjectList/ProjectList.jsx
+++ Frontend/src/pages/ProjectList/ProjectList.jsx
@@ -1,30 +1,120 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
-import { Button } from '@/components/ui/button'
-import { Card, CardContent } from '@/components/ui/card'
-import { MixerHorizontalIcon } from '@radix-ui/react-icons'
-import React from 'react'
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { MagnifyingGlassIcon, MixerHorizontalIcon } from "@radix-ui/react-icons";
+import { Input } from "@/components/ui/input";
+import React from "react";
+
+const tags = [
+  "All",
+  "ReactJS",
+  "NextJS",
+  "Spring Boot",
+  "Java",
+  "Spring",
+  "MySQL",
+  "MongoDB",
+  "Angular",
+  "Python",
+  "NestJS",
+  "Svelte",
+  "JWT",
+  "NodeJS",
+  "Flask",
+  "Django",
+];
 
 const ProjectList = () => {
+  const handleFilterChange = (section, value) => {
+    console.log("value", value, section);
+  };
+  const handleSearchChange=()=>{
+
+  }
   return (
     <>
-    <div className='relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5'>
-      <section className="filterSection">{/* Filter Section*/}
-        <Card className='p-5 sticky top-10 '>
-          <div className='flex justify-between lg:w-[20rem]'>
-            <p className='text-xl -tracking-wider'>Filters</p>
-            <Button variant="ghost"size="icon">
-              <MixerHorizontalIcon />
-            </Button>
+      <div className="relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5">
+        <section className="filterSection">
+          {/* Filter Section*/}
+          <Card className="p-5 sticky top-10 ">
+            <div className="flex justify-between lg:w-[20rem]">
+              <p className="text-xl -tracking-wider">Filters</p>
+              <Button variant="ghost" size="icon">
+                <MixerHorizontalIcon />
+              </Button>
+            </div>
+            <CardContent className="mt-5">
+              <ScrollArea className="space-y-7 h-[70vh] ">
+                <div>
+                  <h1 className="pb-3 text-gray-400 border-b">Category</h1>
+                  <div className="pt-5 ">
+                    <RadioGroup
+                      className="space-y-3 pt-5"
+                      defaultValue="all"
+                      onValueChange={(value) =>
+                        handleFilterChange("category", value)
+                      }
+                    >
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="all" id="r1" />
+                        <Label htmlFor="r1">All</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="fullstack" id="r2" />
+                        <Label htmlFor="r2">Full Stack</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="frontend" id="r3" />
+                        <Label htmlFor="r3">Front End</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="backend" id="r4" />
+                        <Label htmlFor="r4">Back End</Label>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                </div>
+                <div className="pt-9">
+                  <h1 className="pb-3 text-gray-400 border-b">Tags</h1>
+                  <div className="pt-5 ">
+                    <RadioGroup
+                      className="space-y-3 pt-5"
+                      defaultValue="all"
+                      onValueChange={(value) =>
+                        handleFilterChange("tag", value)
+                      }
+                    >
+                      {tags.map((item) => (
+                        <div key={item} className="flex items-center gap-2 ">
+                          <RadioGroupItem value={item} id={`r1-${item}`} />
+                          <Label htmlFor={`r1-${item}`}>{item}</Label>
+                        </div>
+                      ))}
+                    </RadioGroup>
+                  </div>
+                </div>
+              </ScrollArea>
+            </CardContent>
+          </Card>
+        </section>
+        <section className="projectListSection w-full lg:w-[48rem]">
+          {/*Projects Section */}
+          <div className="flex gap-2 items-center pb-5 justify-between">
+            <div className="relative p-0 w-full">
+              <Input 
+              onChange={handleSearchChange}
+              placeholder='Search Project'
+              className="40% px-9" />
+              <MagnifyingGlassIcon className="absolute top-3 left-4" />
+            </div>
           </div>
-          <CardContent className='mt-5'></CardContent>
-        </Card>
-      </section>
-      <section className='projectListSection w-full lg:w-[48rem]'>{/*Projects Section */}
-
-      </section>
-    </div>
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ProjectList
+export default ProjectList;
